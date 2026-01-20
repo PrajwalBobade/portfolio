368f0f8546a5a091520cfb9682ed24ef01401a03
prajwalbobade <prajwalbobade11200@gmail.com>
2026-01-20 17:08:20 +0530

    first commit

---
+++ src/components/footer/config.ts
@@ -0,0 +1,12 @@
+const footer: { title: string; href: string }[] = [
+  {
+    title: "Blog",
+    href: "https://prajwalbobade.hashnode.dev/",
+  },
+  {
+    title: "Newsletter",
+    href: "https://prajwalbobade.hashnode.dev/",
+  },
+];
+
+export { footer };
