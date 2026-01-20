93267dfc98786c4f572649bc63f9b9b0c0346f61
prajwalbobade <prajwalbobade11200@gmail.com>
2026-01-20 17:39:44 +0530

    about fixes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -23,6 +23,7 @@ import {
   RiNextjsFill,
   RiTailwindCssFill,
 } from "react-icons/ri";
+import { BiLogoVisualStudio } from "react-icons/bi";
 import {
   SiExpress,
   SiJavascript,
@@ -31,7 +32,6 @@ import {
   SiPrettier,
   SiTypescript,
   SiVercel,
-  SiVisualstudiocode,
 } from "react-icons/si";
 import { VscCode } from "react-icons/vsc";
 
@@ -167,7 +167,7 @@ const TOOLS = [
   {
     name: "VS Code",
     content: "Next.js is a React framework for production",
-    icon: <SiVisualstudiocode size={"50px"} color="#007acc" />,
+    icon: <BiLogoVisualStudio size={"50px"} color="#007acc" />,
     color: "#007acc",
   },
   {
